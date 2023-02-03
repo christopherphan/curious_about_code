0b9d312ac2a97c93b8d322f77f567604627099ee
Christopher Phan <cphan@chrisphan.com>
2023-02-03 10:29:11 -0600

    Add more functionality to the puzzle module, and an interactive mode

--- 2023-01-29/fifteenpuzzle.py
+++ 2023-01-29/fifteenpuzzle.py
@@ -1,16 +1,31 @@
-"""
-Solution to Part 1 of this puzzle:
-https://discourse.davidamos.dev/t/the-fifteen-puzzle/124
-"""
+"""Validate 15 puzzle arrangments."""
 
+
+from random import choice
 from sys import argv
 from typing import Final
 
 from integerpermutations import IntegerPermutation
 
+VALID_MOVES: Final[frozenset[IntegerPermutation]] = frozenset(
+    # Represents the valid transpositions of the _positions_ (not tiles)
+    # in the puzzle (assuming that one of the two positions swapped is
+    # the empty tile). Right-multiplication by a valid move represents
+    # carrying out the swap on the puzzle arrangment.
+    [IntegerPermutation.transposition(j, j + 4) for j in range(1, 5)]
+    + [IntegerPermutation.transposition(j, j + 4) for j in range(9, 13)]
+    + [IntegerPermutation.transposition(j, j + 1) for j in range(1, 14, 4)]
+    + [IntegerPermutation.transposition(j, j + 1) for j in range(3, 16, 4)]
+)
+
 
 def read_arrangment(text: str) -> IntegerPermutation:
-    """Read the arrangment. The empty space is assigned zero."""
+    """
+    Read the arrangment and return a permutation mapping positions to tiles.
+
+    The empty space is considered tile 16. The positions are indexed by the
+    corresponding tile in the solved puzzle.
+    """
     return IntegerPermutation(
         {
             idx + 1: (16 if val == "E" else int(val))
@@ -28,35 +43,118 @@ def manhattan_distance(x: int) -> int:
     return abs(4 * ((x - 1) // 4 + 1) - x) + abs(3 - (x - 1) // 4)
 
 
-def valid_permutation(x: IntegerPermutation) -> bool:
-    """Return true if x represents a valid arrangement of the 15 puzzle."""
-    return not bool((x.pairity + manhattan_distance((~x)(16))) % 2)
+def empty_position(arrangement: IntegerPermutation) -> int:
+    """Return the position with no tile."""
+    return (~arrangement)(16)
 
 
-def as_puzzle_input(x: IntegerPermutation) -> str:
+# An arrangement is valid if the pairty of the representative permutation and the
+# pairity of the Manhattan distance from empty position to the bottom right corner
+# agree.
+
+
+def valid_permutation(perm: IntegerPermutation) -> bool:
+    """
+    Return true if ``perm`` represents a valid arrangement of the 15 puzzle.
+
+    :param perm: A permutation that maps each position to the tile occupying it.
+    :type perm: IntegerPermutation
+    """
+    return all(k < 17 and k > 0 for k in perm.unfixed) and not bool(
+        ((~perm).pairity + manhattan_distance(empty_position(perm))) % 2
+    )
+
+
+def as_puzzle_input(perm: IntegerPermutation) -> str:
     """Return as text in the style of the puzzle input."""
     return "\n".join(
-        " ".join(str(w) if (w := x(4 * k + j)) != 16 else "E" for j in range(1, 5))
+        " ".join(str(w) if (w := perm(4 * k + j)) != 16 else "E" for j in range(1, 5))
         for k in range(4)
     )
 
 
-if __name__ == "__main__":
-    SAMPLE: Final[
-        str
-    ] = """\
-15 14 13 12
-11 10 9 8
-7 6 5 4
-3 2 1 E"""
+def possible_next_moves(
+    arrangement: IntegerPermutation,
+) -> dict[str, IntegerPermutation]:
+    """Return a the possible next moves given a puzzle arrangement."""
+    empty_pos = empty_position(arrangement)
+    ret_val: dict[str, IntegerPermutation] = {}
+    if empty_pos > 4:
+        # Move empty square up
+        ret_val["up"] = IntegerPermutation.transposition(empty_pos, empty_pos - 4)
+    if empty_pos < 13:
+        # Move empty square down
+        ret_val["down"] = IntegerPermutation.transposition(empty_pos, empty_pos + 4)
+    if (empty_pos % 4) != 0:
+        # move empty square right
+        ret_val["right"] = IntegerPermutation.transposition(empty_pos, empty_pos + 1)
+    if (empty_pos % 4) != 1:
+        # move empty square left
+        ret_val["left"] = IntegerPermutation.transposition(empty_pos, empty_pos - 1)
+    return ret_val
 
+
+def _game_status(arrangement: IntegerPermutation, disp_perm: bool = False) -> str:
+    """Return the board and if the puzzle is solved."""
+    return (
+        (f"E at {empty_position(arrangement)}\n" if disp_perm else "")
+        + "\n".join(
+            " ".join(
+                f"{w:2d}" if (w := arrangement(4 * k + j)) != 16 else " E"
+                for j in range(1, 5)
+            )
+            for k in range(4)
+        )
+        + ("" if arrangement.unfixed else "\n\nPUZZLE IS SOLVED!")
+        + (f"\n{arrangement}" if disp_perm else "")
+    )
+
+
+def scramble(
+    arrangement: IntegerPermutation, num_moves: int = 256
+) -> IntegerPermutation:
+    """Apply random moves."""
+    current = arrangement
+    for _ in range(num_moves):
+        current @= choice(list(possible_next_moves(current).values()))
+    return current
+
+
+def play_puzzle(arrangement: IntegerPermutation | None = None) -> None:
+    """Present iteractive game."""
+    if arrangement is not None and not valid_permutation(arrangement):
+        raise ValueError(
+            "Permutation does not represent a valid arrangment of the fifteen puzzle."
+        )
+    if arrangement is None:
+        current = IntegerPermutation({})
+        rando = input("Scramble pieces (Y/N)? ").strip()
+        if not (rando and rando.upper()[0] == "N"):
+            current = scramble(current)
+    else:
+        current = arrangement
+    disp = input("Display permutations (Y/N)? ").strip()
+    disp_perm = bool(disp) and disp.upper()[0] == "Y"
+    print(_game_status(current, disp_perm))
+    moved: IntegerPermutation | None
+    while k := input("Enter move (U, D, L, R) or a blank input to exit: ").strip():
+        moved = None
+        pnm = possible_next_moves(current)
+        for key, value in pnm.items():
+            if k[0].lower() == key[0].lower():
+                current @= value
+                moved = value
+                break
+        if moved is None:
+            print("Invalid move.")
+        elif disp_perm:
+            print(f"This move: {moved}")
+        print(_game_status(current, disp_perm))
+
+
+if __name__ == "__main__":
     if len(argv) > 1:
         with open(argv[1], "rt") as infile:
-            puzzle_input = infile.read()
+            play_puzzle(read_arrangment(infile.read()))
     else:
-        puzzle_input = SAMPLE
-    perm = read_arrangment(puzzle_input)
-    print(f"Puzzle:\n{puzzle_input}")
-    print(f"Positions -> pieces: {perm}")
-    print(f"Pieces -> positions: {~perm}")
-    print(f"Valid arrangment: {valid_permutation(perm)}")
+        play_puzzle()
